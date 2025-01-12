c6955988c26862e1942e459ed849ada90b538777
prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2025-01-12 15:23:14 -0500

    Add in ad speed functionality

--- application/app.py
+++ application/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # Initialize the S3 client without credentials (will use task role)
 s3 = boto3.client('s3')
-
+adspeedEndpoint = ('https://api.adspeed.com/')
 
 @app.route('/health')
 def health():
@@ -37,6 +37,25 @@ def upload():
             return f"Uploaded Successfully! URL: {file_URL}"
         except Exception as e:
             return f"Error uploading file: {str(e)}"
+
+def sendFormData():
+    
+    if "file" not in request.files:
+        return "No file part"
+    file = request.files["file"]
+    if file.filename == "":
+        return "No selected file"
+    
+    #get adspeed key
+    if file:
+        try:
+            adspeedEndpoint.upload_fileobj(file, file.filename)
+            adspeed = os.getenv('ADSPEED_KEY')
+            file_URL = f"https://api.adspeed.com/"
+            return f"Uploaded to Adspeed!"
+        except Exception as e:
+            return f"Error"
+
 
 
 if __name__ == "__main__":
